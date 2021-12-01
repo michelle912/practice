e9bcb933e77a0bac27c6d247fac38a1dc8ea96bf
michelle <81959777+michelle912@users.noreply.github.com>
2021-12-01 20:55:43 +0800

    implement practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,42 @@
 package practice08;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+
+    private Student leader;
+
+    private List<Student> studentList;
+
+    public Klass(int number) {
+        this.number = number;
+        studentList = new ArrayList<>();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName() {
+        return "Class "+number;
+    }
+
+    public void assignLeader(Student student) {
+        if (!studentList.contains(student)) {
+            System.out.print("It is not one of us.\n");
+            return;
+        }
+        leader = student;
+    }
+
+    public void appendMember(Student student) {
+        studentList.add(student);
+    }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,26 @@
 package practice08;
 
-public class Student {
+
+public class Student extends Person {
+
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+
+        if (klass != null && this.equals(klass.getLeader())) {
+            return super.introduce()+" "+String.format("I am a Student. I am Leader of Class %d.", klass.getNumber());
+        }
+
+        return super.introduce()+" "+String.format("I am a Student. I am at Class %d.", klass.getNumber());
+    }
 }
