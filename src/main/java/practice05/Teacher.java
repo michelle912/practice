6bb8f7fd2ea821c09e4e85f6c02aea6ac643d7a0
michelle <81959777+michelle912@users.noreply.github.com>
2021-12-01 17:38:39 +0800

    implement practice05

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,30 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private Integer klass;
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        this.klass = null;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass == null) {
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+        }
+
+        return super.introduce()+" "+String.format("I am a Teacher. I teach Class %d.", klass);
+    }
+
 }
