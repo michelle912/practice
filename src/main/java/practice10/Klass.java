725f76d60ab75f32330ef966215b7480dbf9858e
michelle <81959777+michelle912@users.noreply.github.com>
2021-12-01 21:53:00 +0800

    wip practice10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,60 @@
 package practice10;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+
+    private Student leader;
+
+    private List<Student> studentList;
+
+    private PropertyChangeSupport support;
+
+    public Klass(int number) {
+        this.number = number;
+        studentList = new ArrayList<>();
+        support = new PropertyChangeSupport(this);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName() {
+        return "Class "+number;
+    }
+
+    public void assignLeader(Student student) {
+        if (!studentList.contains(student)) {
+            System.out.print("It is not one of us.\n");
+            return;
+        }
+        support.firePropertyChange("leader", leader, student);
+        leader = student;
+    }
+
+    public void appendMember(Student student) {
+        support.firePropertyChange("studentList", this.studentList, student);
+        studentList.add(student);
+    }
+
+    public boolean isIn(Student student) {
+        return student.getKlass().getNumber() == number;
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener pcl) {
+        support.addPropertyChangeListener(pcl);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener pcl) {
+        support.removePropertyChangeListener(pcl);
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,60 @@
 package practice10;
 
-public class Teacher {
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person implements PropertyChangeListener {
+
+    private List<Klass> classes;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+        classes = new ArrayList<>();
+    }
+
+    public Teacher(int id, String name, int age, List<Klass> klassList) {
+        super(id, name, age);
+        classes = klassList;
+        klassList.forEach(klass -> klass.addPropertyChangeListener(this));
+    }
+
+    public List<Klass> getClasses() {
+        return classes;
+    }
+
+    @Override
+    public String introduce() {
+        if (classes.isEmpty()) {
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+        }
+
+        List<String> classNumberList = classes.stream().map(Klass::getNumber).map(String::valueOf).collect(Collectors.toList());
+
+        return super.introduce()+" "+String.format("I am a Teacher. I teach Class %s.", String.join(", ", classNumberList));
+    }
+
+    public String introduceWith(Student student) {
+        if (isTeaching(student)) {
+            return super.introduce()+String.format(" I am a Teacher. I teach %s.", student.getName());
+        }
+
+        return super.introduce()+String.format(" I am a Teacher. I don't teach %s.", student.getName());
+    }
+
+    public boolean isTeaching(Student student) {
+        return classes.stream().anyMatch(klass -> klass.isIn(student));
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if (evt.getPropertyName().equals("studentList")) {
+            System.out.printf("I am %s. I know %s has joined Class %d.\n", getName(), evt.getNewValue(), ((Student)evt.getNewValue()).getKlass().getNumber());
+        } else if (evt.getPropertyName().equals("leader")) {
+            System.out.printf("I am %s. I know %s become Leader of Class %d.\n", getName(), evt.getNewValue(), ((Student)evt.getNewValue()).getKlass().getNumber());
+        }
+    }
 }
